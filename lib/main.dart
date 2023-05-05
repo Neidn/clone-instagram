674d2f863807f049d4b4f05bf63d92ebe50c2fba
Neidn <dty3152@gmail.com>
2023-05-05 22:21:03 +0900

    connect firebase with ios, android and web

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:clone_instagram/utils/colors.dart';
 
 import 'package:clone_instagram/responsive/responsive_layout_screen.dart';
 import 'package:clone_instagram/responsive/mobile_screen_layout.dart';
 import 'package:clone_instagram/responsive/web_screen_layout.dart';
+import 'package:clone_instagram/screens/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      name: 'Instagram Clone',
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyBYXov4vZjMaRkU8byuF3JYamy4VIgGAwU',
+        appId: '1:518501392338:web:13061fb93596b08214720b',
+        messagingSenderId: '518501392338',
+        projectId: 'clone-instagram-baae3',
+        storageBucket: 'clone-instagram-baae3.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -22,10 +41,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: ResponsiveLayout(
-        webScreenLayout: WebScreenLayout(),
-        mobileScreenLayout: MobileScreenLayout(),
-      ),
+      // home: ResponsiveLayout(
+        // webScreenLayout: WebScreenLayout(),
+        // mobileScreenLayout: MobileScreenLayout(),
+      // ),
+      home: LoginScreen(),
     );
   }
 }
